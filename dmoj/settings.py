c44559367ec7c83c79b6a76f102d736445915486
Shon Verch <nerftactic09@gmail.com>
2020-02-14 22:41:37 +0000

    Staging

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -30,8 +30,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 SITE_ID = 1
-SITE_NAME = 'DMOJ'
-SITE_LONG_NAME = 'DMOJ: Modern Online Judge'
+SITE_NAME = 'SSOJ'
+SITE_LONG_NAME = 'Stephen Lewis S.S. Online Judge'
 SITE_ADMIN_EMAIL = False
 
 DMOJ_REQUIRE_STAFF_2FA = True
@@ -145,7 +145,7 @@ else:
 
     WPADMIN = {
         'admin': {
-            'title': 'DMOJ Admin',
+            'title': 'SSOJ Admin',
             'menu': {
                 'top': 'wpadmin.menu.menus.BasicTopMenu',
                 'left': 'wpadmin.menu.custom.CustomModelLeftMenuWithDashboard',
---
+++ websocket/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    get_host: '127.0.0.1',
+    get_port: 17100,
+    post_host: '127.0.0.1',
+    post_port: 17101,
+    http_host: '127.0.0.1',
+    http_port: 17102,
+    long_poll_timeout: 29000,
+};
